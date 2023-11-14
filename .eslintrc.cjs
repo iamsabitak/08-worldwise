6bef5f92d9b2b617c91e9b54deb9344610ba7d6f
sabita <sabitakhadka091@gmail.com>
2023-11-14 12:47:32 +0545

    chore: Update ESLint configuration to ignore 'styles' unused variable error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -8,13 +11,18 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: '18.2',
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'no-unused-vars': ['error', { varsIgnorePattern: 'styles' }],
   },
-}
+};
--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
-// import styles from "./PageNav.module"
+import styles from "./PageNav.module.css"
 function PageNav() {
+  // console.log(styles);
   return (
     <nav>
       <ul>

65aafbd40374922b6fc3bb2829c47e6053a19936
sabita <sabitakhadka091@gmail.com>
2023-11-14 13:12:42 +0545

    added css in pageNAV COMPNENT

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,9 +1,8 @@
 import { NavLink } from "react-router-dom";
-import {nav} from "./PageNav.module.css"
+import styles from "./PageNav.module.css";
 function PageNav() {
-  // console.log(styles);
   return (
-    <nav className={nav}>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>

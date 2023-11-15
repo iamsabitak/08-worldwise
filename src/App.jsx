f8e084ef1f96863299a88fb0105029bc56e22a56
sabita <sabitakhadka091@gmail.com>
2023-11-15 15:33:39 +0545

    fetched : the list of cities using a fake api

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,28 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+const BASE_URL = "http://localhost:9000";
 function App() {
   const [cities, setCities] = useState();
   const [isLoading, setIsLoading] = useState();
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        // setCities(data);
+        setCities(data);
+      } catch {
+        alert("ERROR");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
   return (
     <div>
       <BrowserRouter>
@@ -20,8 +37,8 @@ function App() {
           <Route path="pricing" element={<Pricing />} />
           <Route path="app" element={<AppLayout />}>
             <Route index element={<p>LIST</p>} />
-            <Route path="cities" element={<CityList />} />
-            <Route path="countries" element={<CityList />} />
+            <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
+            <Route path="countries" element={<CityList  />} />
             <Route path="form" element={<p>Form</p>} />
           </Route>
           <Route path="/login" element={<Login />} />

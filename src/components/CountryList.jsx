33dbab29baeb5cfd7cada75433753a25986f6721
sabita <sabitakhadka091@gmail.com>
2023-11-16 15:29:03 +0545

    Implementing the Countries List

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -6,9 +6,9 @@ import { useCities } from "../contexts/CitiesContext";
 
 function CountryList() {
   const { cities, isLoading } = useCities();
+  
 
   if (isLoading) return <Spinner />;
-
   if (!cities.length)
     return (
       <Message message="Add your first city by clicking on a city on the map" />

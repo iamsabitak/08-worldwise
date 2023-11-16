0dcf67d22bbdfe091da0719ea66ef97192e9f3e4
sabita <sabitakhadka091@gmail.com>
2023-11-16 15:34:19 +0545

    Implementing the Countries items

--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable react/prop-types */
 import styles from "./CountryItem.module.css";
 
+
+ 
 function CountryItem({ country }) {
+      
   return (
     <li className={styles.countryItem}>
-      <span>{country.emoji}</span>
-      <span>{country.country}</span>
+    <span>{country.emoji}</span>
+    <span>{country.country}</span>
     </li>
   );
 }

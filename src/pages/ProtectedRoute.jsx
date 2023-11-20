75a126dcf768b27474012a4905a278fd31bfeb4a
sabita <sabitakhadka091@gmail.com>
2023-11-20 16:52:29 +0545

    Implement ProtectedRoute for secure page access

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
- /* eslint-disable react/prop-types */
+/* eslint-disable react/prop-types */
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/FakeAuthContext";
